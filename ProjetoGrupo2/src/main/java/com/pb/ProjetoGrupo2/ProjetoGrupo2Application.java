30b34880965c6e6d649f135713870df854fbac34
Gustavo Betete <gustavobetete@hotmail.com>
2022-04-04 14:26:35 -0300

    Iniciando Projeto Final - Grupo 02

---
+++ ProjetoGrupo2/src/main/java/com/pb/ProjetoGrupo2/ProjetoGrupo2Application.java
@@ -0,0 +1,13 @@
+package com.pb.ProjetoGrupo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoGrupo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoGrupo2Application.class, args);
+	}
+
+}
---
+++ ProjetoGrupo2/src/test/java/com/pb/ProjetoGrupo2/ProjetoGrupo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pb.ProjetoGrupo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoGrupo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
