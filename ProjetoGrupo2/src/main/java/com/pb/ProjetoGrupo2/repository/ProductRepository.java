769968967cd3bbd21149c559b1f08fcc6228e2c2
Danilo <danilo_salvato@hotmail.com>
2022-04-12 09:55:13 -0300

    criando um repository para produtos

--- ProjetoGrupo2/src/main/java/com/pb/ProjetoGrupo2/repository/ProductRepository.java
+++ ProjetoGrupo2/src/main/java/com/pb/ProjetoGrupo2/repository/ProductRepository.java
@@ -1,2 +1,13 @@
-package com.pb.ProjetoGrupo2.repository;public interface ProductRepository {
+package com.pb.ProjetoGrupo2.repository;
+
+import com.pb.ProjetoGrupo2.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    void delete(Optional<Product> product);
 }
