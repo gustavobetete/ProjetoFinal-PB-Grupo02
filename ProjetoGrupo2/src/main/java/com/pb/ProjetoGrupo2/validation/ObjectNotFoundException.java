1b09707c66b4f9ed078ee42b7bbee407b370dac5
Danilo <danilo_salvato@hotmail.com>
2022-04-12 09:59:32 -0300

    criando uma exception para controlar o crud

--- ProjetoGrupo2/src/main/java/com/pb/ProjetoGrupo2/validation/ObjectNotFoundException.java
+++ ProjetoGrupo2/src/main/java/com/pb/ProjetoGrupo2/validation/ObjectNotFoundException.java
@@ -1,2 +1,8 @@
-package com.pb.ProjetoGrupo2.validation.ObjectNotFound;public class ObjectNotFoundException {
+package com.pb.ProjetoGrupo2.validation;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
 }
