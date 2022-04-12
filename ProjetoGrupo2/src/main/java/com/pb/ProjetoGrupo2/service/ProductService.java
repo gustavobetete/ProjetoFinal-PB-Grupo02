caab41f68f7630bb6422a6b7048f101c84e9ca5d
Danilo <danilo_salvato@hotmail.com>
2022-04-12 09:56:38 -0300

    criando um service para produtos

--- ProjetoGrupo2/src/main/java/com/pb/ProjetoGrupo2/service/ProductService.java
+++ ProjetoGrupo2/src/main/java/com/pb/ProjetoGrupo2/service/ProductService.java
@@ -1,2 +1,19 @@
-package com.pb.ProjetoGrupo2.service;public interface ProductService {
+package com.pb.ProjetoGrupo2.service;
+
+import com.pb.ProjetoGrupo2.dto.ProductDto;
+import com.pb.ProjetoGrupo2.dto.ProductFormDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ProductService {
+
+    Page<ProductDto> findAll(Pageable page);
+
+    ProductDto save(ProductFormDto ProductFormDto);
+
+    ProductDto search(Long id);
+
+    ProductDto update(Long id, ProductFormDto productFormDto);
+
+    void delete(Long id);
 }
