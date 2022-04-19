4eaf83e21ce48c726ca7c6bbf96f8ef482278d3c
Gustavo Betete <gustavobetete@hotmail.com>
2022-04-19 13:42:08 -0300

    Builder para usar nos testes

---
+++ ProjetoGrupo2/src/test/java/com/pb/ProjetoGrupo2/builder/OrderBuilder.java
@@ -0,0 +1,45 @@
+package com.pb.ProjetoGrupo2.builder;
+
+import com.pb.ProjetoGrupo2.dto.OrderDto;
+import com.pb.ProjetoGrupo2.dto.OrderFormDto;
+import com.pb.ProjetoGrupo2.entities.Order;
+
+import java.sql.Timestamp;
+
+public class OrderBuilder {
+
+    public static Order getOrder() {
+        Order order = new Order();
+        order.setId(1L);
+        order.setQuantity(1);
+        order.setPurchase_date(Timestamp.valueOf("2022-05-23 12:41:00"));
+        order.setDelivery_date(Timestamp.valueOf("2022-05-23 22:00:00"));
+        return order;
+    }
+
+    public static OrderDto getOrderDto() {
+        OrderDto orderDto = new OrderDto();
+        orderDto.setId(1L);
+        orderDto.setQuantity(1);
+        orderDto.setPurchase_date(Timestamp.valueOf("2022-05-23 12:41:00"));
+        orderDto.setDelivery_date(Timestamp.valueOf("2022-05-23 22:00:00"));
+        return orderDto;
+    }
+
+    public static OrderDto getOrderDtoTwo() {
+        OrderDto orderDtoTwo = new OrderDto();
+        orderDtoTwo.setId(2L);
+        orderDtoTwo.setQuantity(2);
+        orderDtoTwo.setPurchase_date(Timestamp.valueOf("2022-05-01 16:23:00"));
+        orderDtoTwo.setDelivery_date(Timestamp.valueOf("2022-05-01 22:00:00"));
+        return orderDtoTwo;
+    }
+
+    public static OrderFormDto getOrderFormDto() {
+        OrderFormDto orderFormDto = new OrderFormDto();
+        orderFormDto.setDelivery_date(Timestamp.valueOf("2022-05-01 22:00:00"));
+        orderFormDto.setPurchase_date(Timestamp.valueOf("2022-05-01 16:23:00"));
+        orderFormDto.setQuantity(10);
+        return orderFormDto;
+    }
+}
